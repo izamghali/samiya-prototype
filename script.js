067616a93c69325f6c60b6e57ecf406a49a39924
Izam Ghali <izamghali18@gmail.com>
2023-05-09 13:59:56 +0700

    add scrollTrigger to the client slide at 90% top

--- script.js
+++ script.js
@@ -1,443 +1,480 @@
-gsap.fromTo("#green", { 
-    x: 200,
-    y: 100,
-    scale: 3,
-}, {
-    ease: "power4.out",
-    scale: 3,
-    x: 700,
-    duration: 2, 
-    delay: 0.6, 
-});
 
-// client slide 
+
+let tween = gsap.fromTo(
+    "#green",
+    {
+      x: 200,
+      y: 100,
+      scale: 3,
+    },
+    {
+      ease: "power4.out",
+      scale: 3,
+      x: 700,
+      duration: 2,
+      delay: 0.6,
+    }
+);
+
+tween.reverse(true);
+
+
+
+// client slide
 function slideThreeImages() {
-    function style1() {
-        let tl1 = gsap.timeline({repeat: -1, repeatDelay: 0, yoyo: true});
-        // style 1
-        tl1.set(".style-1-img-two", {
-            x: '-130%',
-            y: '-100%'
-        },);
-        tl1.set(".style-1-img-three", {
-            y: '-70%'
-        },);
-        tl1.to(".style-1-img-one", { 
-            ease: "power4.inOut",
-            x: '130%',
-            duration: 3, 
-            delay: 1,
-        });
-        tl1.to(".style-1-img-two", { 
-            ease: "power4.inOut",
-            x: '0%',
-            duration: 3, 
-        }, "<");
-        tl1.to(".style-1-img-two", {
-            y: '-230%',
-            ease: "power4.inOut",
-            duration: 3, 
-        })
-        tl1.to(".style-1-img-three", { 
-            ease: "power4.inOut",
-            y: '-200%',
-            duration: 3, 
-        }, "<");
-    }
-    
-    function style2() {
-        let tl2 = gsap.timeline({repeat: -1, repeatDelay: 0, yoyo: true});
-        tl2.set(".style-2-img-two", {
-            x: '130%',
-            y: '-100%',
-        },);
-        tl2.set(".style-2-img-three", {
-            y: '-330%',
-        },);
-        tl2.to(".style-2-img-one", { 
-            ease: "power4.inOut",
-            x: '-130%',
-            duration: 3,
-            delay: 3, 
-        });
-        tl2.to(".style-2-img-two", { 
-            ease: "power4.inOut",
-            x: '0%',
-            duration: 3, 
-        }, "<");
-        tl2.to(".style-2-img-two", {
-            y: '30%',
-            ease: "power4.inOut",
-            duration: 3, 
-        })
-        tl2.to(".style-2-img-three", { 
-            ease: "power4.inOut",
-            y: '-200%',
-            duration: 3, 
-        }, "<");
+  function style1() {
+    let tl1 = gsap.timeline({ scrollTrigger: {
+        trigger: ".style-1-img-two", 
+        start: "top 90%",
+    }, repeat: -1, yoyo: true, delay: 1.5, defaults: {
+        duration: 3,
+        ease: "power4.inOut",
+    }});
+    // style 1
+    tl1.set(".style-1-img-two", {
+      x: "-130%",
+      y: "-100%",
+    });
+    tl1.set(".style-1-img-three", {
+      y: "-70%",
+    });
+    tl1.to(".style-1-img-one", {
+      x: "130%",
 
-    }
+    });
+    tl1.to(
+      ".style-1-img-two",
+      {
+        x: "0%",
+      },
+      "<"
+    );
+    tl1.to(".style-1-img-two", {
+      y: "-230%",
+    });
+    tl1.to(
+      ".style-1-img-three",
+      {
+        y: "-200%",
+      },
+      "<"
+    );
+  }
 
-    style1();
-    style2();
+  function style2() {
+    let tl2 = gsap.timeline({ scrollTrigger: {
+        trigger: ".style-2-img-two", 
+        start: "top 90%",
+    }, repeat: -1, yoyo: true, delay: 0.3, defaults: {
+        duration: 3,
+        ease: "power4.inOut",
+    } });
+    tl2.set(".style-2-img-two", {
+      x: "130%",
+      y: "-100%",
+    });
+    tl2.set(".style-2-img-three", {
+      y: "-330%",
+    });
+    tl2.to(".style-2-img-one", {
+      x: "-130%",
+    });
+    tl2.to(
+      ".style-2-img-two",
+      {
+        x: "0%",
+      },
+      "<"
+    );
+    tl2.to(".style-2-img-two", {
+      y: "30%",
+    });
+    tl2.to(
+      ".style-2-img-three",
+      {
+        y: "-200%",
+      },
+      "<"
+    );
+  }
 
+  style1();
+  style2();
 }
 
 function slideFourImages() {
+  function style3() {
+    let tl3 = gsap.timeline({ scrollTrigger: {
+        trigger: ".style-3-img-two", 
+        start: "top 90%",
+    }, repeat: -1, yoyo: true, defaults: {
+        duration: 3,
+        ease: "power4.inOut",
+    } });
 
-    function style3() {
-        let tl3 = gsap.timeline({repeat: -1, repeatDelay: 0, yoyo: true});
-        
-        // initial state
-        tl3.set(".style-3-img-two", {
-            x: '130%',
-            y: '-100%'
-        },);
-        tl3.set(".style-3-img-three", {
-            y: '-200%',
-            x: '130%',
-        });
-        tl3.set(".style-3-img-four", {
-            x: '0%',
-            y: '-430%'
-        })
-        
-        // 1# motion
-        tl3.to(".style-3-img-one", { 
-            x: '-130%',
-            ease: "power4.inOut",
-            duration: 3, 
-            delay: 2
-        });
-        tl3.to(".style-3-img-two", { 
-            x: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // 2# motion
-        tl3.to(".style-3-img-one", {
-            x: '-260%',
-            ease: "power4.inOut",
-            duration: 3, 
-        });
-        tl3.to(".style-3-img-two", {
-            x: '-130%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        tl3.to(".style-3-img-three", { 
-            x: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // 3# motion
-        tl3.to(".style-3-img-three", {
-            y: '-70%',
-            ease: "power4.inOut",
-            duration: 3, 
-        })
-        tl3.to(".style-3-img-four", {
-            y: '-300%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<")
+    // initial state
+    tl3.set(".style-3-img-two", {
+      x: "130%",
+      y: "-100%",
+    });
+    tl3.set(".style-3-img-three", {
+      y: "-200%",
+      x: "130%",
+    });
+    tl3.set(".style-3-img-four", {
+      x: "0%",
+      y: "-430%",
+    });
 
-    }
+    // 1# motion
+    tl3.to(".style-3-img-one", {
+      x: "-130%",
+    });
+    tl3.to(
+      ".style-3-img-two",
+      {
+        x: "0%",
+      },
+      "<"
+    );
 
-    function style4() {
-        let tl4 = gsap.timeline({repeat: -1, repeatDelay: 0, yoyo: true});
-            
-        // // initial state
-        tl4.set(".style-4-img-two", {
-            x: '0%',
-            y: '30%'
-        },);
-        tl4.set(".style-4-img-three", {
-            y: '-70%',
-            x: '0%',
-        });
-        tl4.set(".style-4-img-four", {
-            x: '0%',
-            y: '-170%'
-        })
-        
-        // // 1# motion
-        tl4.to(".style-4-img-one", { 
-            y: '-130%',
-            ease: "power4.inOut",
-            duration: 3, 
-            // delay: 5
-        });
-        tl4.to(".style-4-img-two", { 
-            y: '-100%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // // 2# motion
-        tl4.to(".style-4-img-two", {
-            y: '-230%',
-            ease: "power4.inOut",
-            duration: 3, 
-        });
-        tl4.to(".style-4-img-three", { 
-            y: '-200%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // 3# motion
-        tl4.set(".style-4-img-one", {
-            y: '130%',
-            ease: "power4.inOut",
-            duration: 3, 
-        })
-        tl4.to(".style-4-img-four", {
-            y: '-300%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<")
-        tl4.to(".style-4-img-three", { 
-            y: '-330%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        tl4.to(".style-4-img-one", {
-            y: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        },)
-        tl4.to(".style-4-img-four", {
-            y: '-430%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<")
+    // 2# motion
+    tl3.to(".style-3-img-one", {
+      x: "-260%",
+    });
+    tl3.to(
+      ".style-3-img-two",
+      {
+        x: "-130%",
+      },
+      "<"
+    );
+    tl3.to(
+      ".style-3-img-three",
+      {
+        x: "0%",
+      },
+      "<"
+    );
 
-    }
+    // 3# motion
+    tl3.to(".style-3-img-three", {
+      y: "-70%",
+    });
+    tl3.to(
+      ".style-3-img-four",
+      {
+        y: "-300%",
+      },
+      "<"
+    );
+  }
 
-    function style5() {
-        let tl5 = gsap.timeline({repeat: -1, repeatDelay: 0});
-            
-        // // initial state
-        tl5.set(".style-5-img-two", {
-            x: '0%',
-            y: '-230%'
-        },);
-        tl5.set(".style-5-img-three", {
-            y: '-330%',
-            x: '0%',
-        });
-        tl5.set(".style-5-img-four", {
-            x: '0%',
-            y: '-430%'
-        })
-        
-        // // 1# motion
-        tl5.to(".style-5-img-one", { 
-            y: '130%',
-            ease: "power4.inOut",
-            duration: 3, 
-            delay: 4
-        });
-        tl5.to(".style-5-img-two", { 
-            y: '-100%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // // 2# motion
-        tl5.to(".style-5-img-two", {
-            y: '30%',
-            ease: "power4.inOut",
-            duration: 3, 
-        });
-        tl5.to(".style-5-img-three", { 
-            y: '-200%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // 3# motion
-        tl5.set(".style-5-img-one", {
-            y: '-130%',
-            ease: "power4.inOut",
-            duration: 3, 
-        })
-        tl5.to(".style-5-img-four", {
-            y: '-300%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<")
-        tl5.to(".style-5-img-three", { 
-            y: '-70%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        tl5.to(".style-5-img-one", {
-            y: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        },)
-        tl5.to(".style-5-img-four", {
-            y: '-170%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<")
+  function style4() {
+    let tl4 = gsap.timeline({ scrollTrigger: {
+        trigger: ".style-4-img-two", 
+        start: "top 90%",
+    }, repeat: -1, yoyo: true, delay: 0.6, defaults: {
+        duration: 3,
+        ease: "power4.inOut",
+    }});
 
-    }
+    // // initial state
+    tl4.set(".style-4-img-two", {
+      x: "0%",
+      y: "30%",
+    });
+    tl4.set(".style-4-img-three", {
+      y: "-70%",
+      x: "0%",
+    });
+    tl4.set(".style-4-img-four", {
+      x: "0%",
+      y: "-170%",
+    });
 
-    function style6() {
-        let tl6 = gsap.timeline({repeat: -1, repeatDelay: 0, yoyo: true});
-            
-        // // initial state
-        tl6.set(".style-6-img-two", {
-            y: '-100%',
-            x: '-130%'
-        },);
-        tl6.set(".style-6-img-three", {
-            y: '-200%',
-            x: '-130%',
-        });
-        tl6.set(".style-6-img-four", {
-            x: '-130%',
-            y: '-300%'
-        })
-        
-        // // 1# motion
-        tl6.to(".style-6-img-one", { 
-            x: '130%',
-            ease: "power4.inOut",
-            duration: 3, 
-            delay: 5
-        });
-        tl6.to(".style-6-img-two", { 
-            x: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // // 2# motion
-        tl6.to(".style-6-img-two", {
-            x: '130%',
-            ease: "power4.inOut",
-            duration: 3, 
-        });
-        tl6.to(".style-6-img-three", { 
-            x: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
-        
-        // 3# motion
-        tl6.set(".style-6-img-one", {
-            y: '-130%',
-            ease: "power4.inOut",
-            duration: 3, 
-        })
-        tl6.to(".style-6-img-four", {
-            x: '0%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<")
-        tl6.to(".style-6-img-three", { 
-            x: '130%',
-            ease: "power4.inOut",
-            duration: 3, 
-        }, "<");
+    // // 1# motion
+    tl4.to(".style-4-img-one", {
+      y: "-130%",
+    });
+    tl4.to(
+      ".style-4-img-two",
+      {
+        y: "-100%",
+      },
+      "<"
+    );
 
-    }
+    // // 2# motion
+    tl4.to(".style-4-img-two", {
+      y: "-230%",
+    });
+    tl4.to(
+      ".style-4-img-three",
+      {
+        y: "-200%",
+      },
+      "<"
+    );
 
-    style3();
-    style4();
-    style5();
-    style6();
+    // 3# motion
+    tl4.set(".style-4-img-one", {
+      y: "130%",
+    });
+    tl4.to(
+      ".style-4-img-four",
+      {
+        y: "-300%",
+      },
+      "<"
+    );
+    tl4.to(
+      ".style-4-img-three",
+      {
+        y: "-330%",
+      },
+      "<"
+    );
 
+    tl4.to(".style-4-img-one", {
+      y: "0%",
+    });
+    tl4.to(
+      ".style-4-img-four",
+      {
+        y: "-430%",
+      },
+      "<"
+    );
+  }
+
+  function style5() {
+    let tl5 = gsap.timeline({ scrollTrigger: {
+        trigger: ".style-5-img-two", 
+        start: "top 90%",
+    }, repeat: -1, delay: 1, defaults: {
+        duration: 3,
+        ease: "power4.inOut",
+    }});
+
+    // // initial state
+    tl5.set(".style-5-img-two", {
+      x: "0%",
+      y: "-230%",
+    });
+    tl5.set(".style-5-img-three", {
+      y: "-330%",
+      x: "0%",
+    });
+    tl5.set(".style-5-img-four", {
+      x: "0%",
+      y: "-430%",
+    });
+
+    // // 1# motion
+    tl5.to(".style-5-img-one", {
+      y: "130%",
+    });
+    tl5.to(
+      ".style-5-img-two",
+      {
+        y: "-100%",
+      },
+      "<"
+    );
+
+    // // 2# motion
+    tl5.to(".style-5-img-two", {
+      y: "30%",
+    });
+    tl5.to(
+      ".style-5-img-three",
+      {
+        y: "-200%",
+      },
+      "<"
+    );
+
+    // 3# motion
+    tl5.set(".style-5-img-one", {
+      y: "-130%",
+    });
+    tl5.to(
+      ".style-5-img-four",
+      {
+        y: "-300%",
+      },
+      "<"
+    );
+    tl5.to(
+      ".style-5-img-three",
+      {
+        y: "-70%",
+      },
+      "<"
+    );
+
+    tl5.to(".style-5-img-one", {
+      y: "0%",
+    });
+    tl5.to(
+      ".style-5-img-four",
+      {
+        y: "-170%",
+      },
+      "<"
+    );
+  }
+
+  function style6() {
+    let tl6 = gsap.timeline({ scrollTrigger: {
+        trigger: ".style-6-img-two", 
+        start: "top 90%",
+    }, repeat: -1, yoyo: true, delay: 1.2, defaults: {
+        duration: 3,
+        ease: "power4.inOut",
+    }});
+
+    // // initial state
+    tl6.set(".style-6-img-two", {
+      y: "-100%",
+      x: "-130%",
+    });
+    tl6.set(".style-6-img-three", {
+      y: "-200%",
+      x: "-130%",
+    });
+    tl6.set(".style-6-img-four", {
+      x: "-130%",
+      y: "-300%",
+    });
+
+    // // 1# motion
+    tl6.to(".style-6-img-one", {
+      x: "130%",
+    });
+    tl6.to(
+      ".style-6-img-two",
+      {
+        x: "0%",
+      },
+      "<"
+    );
+
+    // // 2# motion
+    tl6.to(".style-6-img-two", {
+      x: "130%",
+    });
+    tl6.to(
+      ".style-6-img-three",
+      {
+        x: "0%",
+      },
+      "<"
+    );
+
+    // 3# motion
+    tl6.set(".style-6-img-one", {
+      y: "-130%",
+    });
+    tl6.to(
+      ".style-6-img-four",
+      {
+        x: "0%",
+      },
+      "<"
+    );
+    tl6.to(
+      ".style-6-img-three",
+      {
+        x: "130%",
+      },
+      "<"
+    );
+  }
+
+  style3();
+  style4();
+  style5();
+  style6();
 }
+
 
 slideThreeImages();
 slideFourImages();
 
-
 // GSAP matchMedia
 let mm = gsap.matchMedia(),
-    breakPoint = 768;
+  breakPoint = 768;
 
-mm.add({
-
+mm.add(
+  {
     // set up any number of arbitrarily-named conditions. The function below will be called when ANY of them match.,
     isDesktop: `(min-width: ${breakPoint}px)`,
     isMobile: `(max-width: ${breakPoint - 1}px)`,
-    reduceMotion: "(prefers-reduced-motion: reduce)"
-  
-  }, (context) => {
-  
+    reduceMotion: "(prefers-reduced-motion: reduce)",
+  },
+  (context) => {
     // context.conditions has a boolean property for each condition defined above indicating if it's matched or not.
     let { isDesktop, isMobile, isTablet } = context.conditions;
-  
+
     // carousel zoom-in
-    gsap.fromTo(".carousel-img", { 
-        backgroundSize: isDesktop ? '100%' : '410%',
-    }, {
+    gsap.fromTo(
+      ".carousel-img",
+      {
+        backgroundSize: isDesktop ? "100%" : "410%",
+      },
+      {
         ease: "power1.inOut",
         duration: 12,
         yoyo: true,
-        backgroundSize: isDesktop ? '130%' : '500%',
+        backgroundSize: isDesktop ? "130%" : "500%",
         repeat: -1,
-    })
+      }
+    );
 
-    return () => { 
-        // optional
+    return () => {
+      // optional
     };
-        
-}); 
+  }
+);
 
 // jQuery
 $(document).ready(() => {
-  
-    // bts collapse
-    $('#btsCollapse1').on("click", () => {
-        // $('#btsCollapseText1').css("display", "block");
-        $('#btsCollapseText1').slideToggle(250);
-        $('#btsCollapseText2').slideUp(250);
-        $('#btsCollapseText3').slideUp(250);
-        
-    })
-    
-    $('#btsCollapse2').on("click", () => {
-        $('#btsCollapseText2').slideToggle(250);
-        $('#btsCollapseText1').slideUp(250);
-        $('#btsCollapseText3').slideUp(250);
+  // bts collapse
+  $("#btsCollapse1").on("click", () => {
+    // $('#btsCollapseText1').css("display", "block");
+    $("#btsCollapseText1").slideToggle(250);
+    $("#btsCollapseText2").slideUp(250);
+    $("#btsCollapseText3").slideUp(250);
+  });
 
-    })
+  $("#btsCollapse2").on("click", () => {
+    $("#btsCollapseText2").slideToggle(250);
+    $("#btsCollapseText1").slideUp(250);
+    $("#btsCollapseText3").slideUp(250);
+  });
 
-    $('#btsCollapse3').on("click", () => {
-        $('#btsCollapseText3').slideToggle(250);
-        $('#btsCollapseText1').slideUp(250);
-        $('#btsCollapseText2').slideUp(250);
+  $("#btsCollapse3").on("click", () => {
+    $("#btsCollapseText3").slideToggle(250);
+    $("#btsCollapseText1").slideUp(250);
+    $("#btsCollapseText2").slideUp(250);
+  });
 
-    })
+  $("#green").on("click", (event) => {
+    $(event.currentTarget).hide();
+  });
 
-    $('#green').on("click", (event) => {
-        $(event.currentTarget).hide();
-    })
+  $("#footerHouseFill").hide();
 
-    $('#footerHouseFill').hide();
-    
-    // footer house icon
-    $('#footerHouseLink').on("mouseenter", () => {
-        $('#footerHouseFill').show();
-        $('#footerHouseNoFill').hide();
-    })
-    
-    $('#footerHouseFill').on("mouseleave", () => {
-        $('#footerHouseNoFill').show();
-        $('#footerHouseFill').hide();
-        
-    })
+  // footer house icon
+  $("#footerHouseLink").on("mouseenter", () => {
+    $("#footerHouseFill").show();
+    $("#footerHouseNoFill").hide();
+  });
 
-  
+  $("#footerHouseFill").on("mouseleave", () => {
+    $("#footerHouseNoFill").show();
+    $("#footerHouseFill").hide();
+  });
 });
