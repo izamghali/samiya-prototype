e18e9bff89e3835911ec2f50bbd16e33dd80abc1
izamghali <izamghali18@gmail.com>
2023-06-05 11:42:37 +0700

    added click function on orientation button

--- gallery-script.js
+++ gallery-script.js
@@ -1,15 +1,42 @@
-
-
-
 let standardBlackBg = "#1c1c1c";
 let standardWhiteBg = "hsl(0, 0%, 90%)";
 
-let galleryContactButton = document.getElementById("galleryContactButton");
+let lightColor = "#ADADAD"
 
-galleryContactButton.addEventListener("mouseenter", () => {
-    gsap.to("#galleryContactButton", {backgroundColor: standardWhiteBg, color: standardBlackBg})
-})
+let buttonState = true;
 
-galleryContactButton.addEventListener("mouseleave", () => {
-    gsap.to("#galleryContactButton", {backgroundColor: standardBlackBg, color: standardWhiteBg})
-})
+let galleryCheckBoxOrientation = document.getElementById("galleryCheckBoxOrientation");
+
+
+function buttonClick() {
+    if (buttonState) {
+        galleryCheckBoxOrientation.addEventListener("click", () => {
+            let buttonTl = gsap.timeline()
+            gsap.set("#galleryCheckBoxOrientationList a", { color: standardBlackBg, visibility: "hidden" });
+            buttonTl.fromTo("#galleryCheckBoxOrientationList", { height: "0rem" }, {height: "auto", opacity: 1, duration: 0.4});
+            buttonTl.to("#galleryCheckBoxOrientationList a", { color: lightColor, visibility: "visible"});
+            buttonTl.to("#galleryCheckBoxOrientationList a", { cursor: "pointer" });
+        });
+    } else {
+        galleryCheckBoxOrientation.addEventListener("click", () => {
+            gsap.to("#galleryCheckBoxOrientationList", { opacity: 0, duration: 2 });
+            gsap.to("#galleryCheckBoxOrientationList a", { cursor: "default" });
+        });
+    }
+    buttonState = false;
+}
+
+buttonClick();
+
+$(document).ready(function() {
+    
+    $("#galleryCheckBoxOrientationList a").on("mouseenter", (event) => {
+        $(event.currentTarget).css("background", standardWhiteBg)
+        $(event.currentTarget).css("color", standardBlackBg)
+    })
+    $("#galleryCheckBoxOrientationList a").on("mouseleave", (event) => {
+        $(event.currentTarget).css("background", standardBlackBg)
+        $(event.currentTarget).css("color", lightColor)
+    })
+    
+});
