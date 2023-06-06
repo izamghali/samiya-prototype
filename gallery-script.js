d1912c3da52e2e829cc491cbe4cf69f1d26e56c1
izamghali <izamghali18@gmail.com>
2023-06-06 16:52:53 +0700

    added category highlight click effect on jQuery

--- gallery-script.js
+++ gallery-script.js
@@ -7,6 +7,7 @@ let lightColor = "#ADADAD"
 
 $(document).ready(function() {
     
+    // hover contact btn
     $("#galleryCheckBoxOrientationList a").on("mouseenter", (event) => {
         $(event.currentTarget).css("background", standardWhiteBg)
         $(event.currentTarget).css("color", standardBlackBg)
@@ -14,6 +15,15 @@ $(document).ready(function() {
     $("#galleryCheckBoxOrientationList a").on("mouseleave", (event) => {
         $(event.currentTarget).css("background", standardBlackBg)
         $(event.currentTarget).css("color", lightColor)
+    })
+
+    // category highlight click
+    $("#galleryCheckBoxCategory a").not("#categoryAll").on("click", (event) => {
+        $("#galleryCheckBoxCategory a").not(event.currentTarget).fadeTo("50", 0.4);
+        $(event.currentTarget).fadeTo("50", 1);
+    })
+    $("#categoryAll").on("click", () => {
+        $("#galleryCheckBoxCategory a").fadeTo("50", 1)
     })
     
 });
