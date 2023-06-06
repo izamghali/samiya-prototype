11f1340578968bc87143b7041284e88b87363862
izamghali <izamghali18@gmail.com>
2023-06-06 16:36:55 +0700

    changed checkbox from a tag to select & option tag

--- gallery-script.js
+++ gallery-script.js
@@ -3,30 +3,7 @@ let standardWhiteBg = "hsl(0, 0%, 90%)";
 
 let lightColor = "#ADADAD"
 
-let buttonState = true;
 
-let galleryCheckBoxOrientation = document.getElementById("galleryCheckBoxOrientation");
-
-
-function buttonClick() {
-    if (buttonState) {
-        galleryCheckBoxOrientation.addEventListener("click", () => {
-            let buttonTl = gsap.timeline()
-            gsap.set("#galleryCheckBoxOrientationList a", { color: standardBlackBg, visibility: "hidden" });
-            buttonTl.fromTo("#galleryCheckBoxOrientationList", { height: "0rem" }, {height: "auto", opacity: 1, duration: 0.4});
-            buttonTl.to("#galleryCheckBoxOrientationList a", { color: lightColor, visibility: "visible"});
-            buttonTl.to("#galleryCheckBoxOrientationList a", { cursor: "pointer" });
-        });
-    } else {
-        galleryCheckBoxOrientation.addEventListener("click", () => {
-            gsap.to("#galleryCheckBoxOrientationList", { opacity: 0, duration: 2 });
-            gsap.to("#galleryCheckBoxOrientationList a", { cursor: "default" });
-        });
-    }
-    buttonState = false;
-}
-
-buttonClick();
 
 $(document).ready(function() {
     
