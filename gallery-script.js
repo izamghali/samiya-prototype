89184333cecc89ed291fd4a6f4bbd44e14dd3cfa
izamghali <izamghali18@gmail.com>
2023-06-07 16:38:24 +0700

    added loader effect for opening. Need to add grid filter

--- gallery-script.js
+++ gallery-script.js
@@ -2,6 +2,14 @@ let standardBlackBg = "#1c1c1c";
 let standardWhiteBg = "hsl(0, 0%, 90%)";
 let lightColor = "#ADADAD"
 
+function gridLoader() {
+    gsap.from(".grid-container", { y: 100, delay: 0.2, opacity: 0 });
+    gsap.from("#galleryHeading", { y: -100, delay: 0.2, opacity: 0 });
+    gsap.from("#galleryCheckBox", { y: -100, delay: 0.2, opacity: 0 });
+}
+
+gridLoader();
+
 $(document).ready(function() {
     
     // hover contact btn
@@ -18,9 +26,15 @@ $(document).ready(function() {
     $("#galleryCheckBoxCategory a").not("#categoryAll").on("click", (event) => {
         $("#galleryCheckBoxCategory a").not(event.currentTarget).fadeTo("50", 0.4);
         $(event.currentTarget).fadeTo("50", 1);
+        const $filterText = event.currentTarget.textContent;
+        // if ($filterText === "COMPANY") {
+
+        // }
     })
     $("#categoryAll").on("click", () => {
         $("#galleryCheckBoxCategory a").fadeTo("50", 1)
     })
     
 });
+
+
